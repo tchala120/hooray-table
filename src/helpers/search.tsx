76d5a734c20d46b885cb829797629d2f226108ae
Panupong Tipjoi <panupong.ti@hotmail.com>
2021-11-25 23:40:32 +0700

    feat: Add dropdown filter type option handler

--- src/helpers/search.tsx
+++ src/helpers/search.tsx
@@ -1,15 +1,18 @@
 import React from "react"
 import { Input } from 'antd'
 
-import TableFilterDropDown from "../components/TableFilterDropDown"
+import TableFilterDropDown, { DropDownFitlerType } from "../components/TableFilterDropDown"
 import FontAwesomeIcon from "../components/FontAwesomeIcon"
 
 import type { MutableRefObject } from 'react'
 import type { ColumnType } from 'antd/lib/table'
+import type { LocaleType } from "./locale"
 
 export interface ColumnSearchPropsGetterOptions {
+  locale?: LocaleType
   dataIndex: string
   searchInputRef: MutableRefObject<Input | null | undefined>
+  setDropDownFilterType?: (filterType: DropDownFitlerType) => void
 }
 
 export const getColumnSearchProps = (options: ColumnSearchPropsGetterOptions): ColumnType<any> => {
@@ -17,7 +20,7 @@ export const getColumnSearchProps = (options: ColumnSearchPropsGetterOptions): C
 
   return {
     filterDropdown: ({ prefixCls, filters, visible, ...props }) => (
-      <TableFilterDropDown {...props} searchInputRef={searchInputRef} locale="enUS" />
+      <TableFilterDropDown {...props} searchInputRef={searchInputRef} locale={options.locale} setDropDownFilterType={options.setDropDownFilterType}/>
     ),
     filterIcon: (filtered) => <FontAwesomeIcon iconName="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
     onFilterDropdownVisibleChange: (visible) => {
